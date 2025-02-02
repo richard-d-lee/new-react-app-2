db11f33515c61f4f24d2fb6c14b23d6b6f7121ed
richard-d-lee <richdelee@gmail.com>
2025-02-01 21:30:10 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // Entry point for your app
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js', // Output file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Transpile .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/, // Handle CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // HTML template
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'), // Serve files from the dist directory
+    port: 3000, // Port for the development server
+    open: true, // Open the browser automatically
+  },
+};
